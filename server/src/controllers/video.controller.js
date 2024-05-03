445b52ac678b8a1956bc7e6619b978289b2cdb17
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-03 17:41:39 +0300

    Update

--- server/src/controllers/video.controller.js
+++ server/src/controllers/video.controller.js
@@ -1,36 +1,24 @@
 import responseHandler from "../handlers/response.handler.js";
 import videoModel from "../models/video.model.js";
-import cloudinary from 'cloudinary'
+import multer from 'multer';
+
+// Configure multer for handling file uploads
+const upload = multer({ dest: 'videos/' }); // Adjust the destination directory as needed
 
 const addVideo = async (req, res) => {
   try {
-    console.log(req)
-    if (!req.body || !req.body.video) {
+    if (!req.file) {
       return res.status(400).json({ error: 'No file provided in the request' });
     }
 
     const video = new videoModel({
       title: req.body.title || 'Untitled Video',
-      video: req.body.video,
+      video: req.file.path, // Assuming req.file contains the uploaded video file
     });
 
     await video.save();
 
     res.status(201).json({ message: 'Video uploaded successfully'});
-    // await cloudinary.v2.uploader.upload_stream({ resource_type: 'video' }, async (error, result) => {
-    //   if (error) {
-    //     console.error('Error uploading to Cloudinary:', error);
-    //     return res.status(500).json({ error: 'Error uploading to Cloudinary' });
-    //   }
-    //   const video = new videoModel({
-    //     title: req.body.title || 'Untitled Video',
-    //     video: result.secure_url,
-    //   });
-
-    //   await video.save();
-
-    //   res.status(201).json({ message: 'Video uploaded successfully'});
-    // }).end(req.body.video);
   } catch (error) {
     console.error('Error uploading video:', error);
     res.status(500).json({ error: 'Error uploading video' });
@@ -39,10 +27,10 @@ const addVideo = async (req, res) => {
 
 const getVideo = async (req, res) => {
   try {
-    const video = await video.find({ user: req.user.id }).sort("-createdAt");
-
-    responseHandler.ok(res, video);
-  } catch {
+    const videos = await videoModel.find({ user: req.user.id }).sort("-createdAt");
+    responseHandler.ok(res, videos);
+  } catch (error) {
+    console.error('Error retrieving videos:', error);
     responseHandler.error(res);
   }
 };
