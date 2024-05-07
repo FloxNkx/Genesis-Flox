4753ed3fd5213c216fa43cc9528649dcbbd3e03a
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-07 18:34:50 +0300

    Update

--- server/index.js
+++ server/index.js
@@ -8,39 +8,39 @@ import cors from 'cors';
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
-const server = http.createServer(app);
-
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log("Mongodb connected");
   server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://genesis-lovat.vercel.app');
-    res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-    res.header('Access-Control-Allow-Headers', "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
-    res.header('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-    };
-    next();
-  });
-
-  app.set("view engine", "ejs");
-  app.use("/", (req, res) => res.send('Hewllo'));
-  app.use("/api/v1", routes);
 }).catch((err) => {
   console.log({ err });
   process.exit(1);
 });
 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://genesis-lovat.vercel.app');
+  res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.header('Access-Control-Allow-Headers', "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+  res.status(200).end();
+  return;
+  };
+  next();
+});
+
+app.set("view engine", "ejs");
+app.use("/", (req, res) => res.send('Hewllo'));
+app.use("/api/v1", routes)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
 
 export default app
