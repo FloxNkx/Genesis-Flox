5ac609c753b7e91ecdce0b3d59b5089da68583d0
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-07 18:38:29 +0300

    Update

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,7 +3,7 @@ import videoRoute from "./video.route.js";
 
 const router = express.Router();
 
-// router.use("/main", videoRoute);
+router.use("/main", videoRoute);
 router.get('/', function(req, res) {
     res.send('Birds home page');
 });
