4ed9e14ca9be2d1d575db8603b39895aabc9510d
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-07 18:01:53 +0300

    Update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://genesis-beige.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'https://genesis-lovat.vercel.app');
   res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT");
   res.header('Access-Control-Allow-Headers', "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
   res.header('Access-Control-Allow-Credentials', 'true');
