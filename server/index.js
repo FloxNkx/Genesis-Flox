52c02433ce331d2fa914e21a56b3d2f1cdaf6b1e
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-07 18:16:15 +0300

    Update

--- server/index.js
+++ server/index.js
@@ -8,25 +8,6 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://genesis-lovat.vercel.app');
-  res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-  res.header('Access-Control-Allow-Headers', "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-  res.status(200).end();
-  return;
-  };
-  next();
-});
-
-app.set("view engine", "ejs");
-app.use("/api/v1", routes);
-
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
@@ -36,9 +17,29 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
   server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://genesis-lovat.vercel.app');
+    res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.header('Access-Control-Allow-Headers', "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+    };
+    next();
+  });
+
+  app.set("view engine", "ejs");
+  app.use("/api/v1", routes);
 }).catch((err) => {
   console.log({ err });
   process.exit(1);
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
 
 export default app
