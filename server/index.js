34ef0cb367a1c7f8b5f0ef0240784d4c17350db1
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-07 16:46:01 +0300

    Update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use((req, res, next) => {  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'false');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json(({ body: 'OK' }));
+  }
+
+  return next();
+})
+
 app.set("view engine", "ejs");
 app.use("/api/v1", routes);
 
