f160c60f1ef559d087dbddb7d258c3d55d18fbd2
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-07 16:41:12 +0300

    Update

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
 import routes from "./src/routes/index.js";
 
 const app = express();
-
-// app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
