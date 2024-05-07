9692ab2c20d7649fb5439a332f557f55d99eab7c
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-07 18:25:11 +0300

    Update

--- server/src/routes/video.route.js
+++ server/src/routes/video.route.js
@@ -7,6 +7,11 @@ const router = express.Router();
 
 let connection = mongoose.connection;
 
+router.get('/ver', function (req, res, next) {
+  res.send('POST request to the homepage')
+  res.end();
+})
+
 connection.on("open", () => {
   console.log("connection established successfully");
   let bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
@@ -14,6 +19,11 @@ connection.on("open", () => {
   const storage = multer.memoryStorage();
   const upload = multer({ storage });
 
+  router.get('/veri', function (req, res, next) {
+    res.send('POST request to the homepage')
+    res.end();
+  })
+  
   router.get("/video/:fileId", upload.single("file"), (req, res) =>
     videoController.getVideo(req, res, bucket)
   );
