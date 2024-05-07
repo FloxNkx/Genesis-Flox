6cbb849b5e5f1f1c0ce6f38eedb864e085501dd2
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-07 16:52:17 +0300

    Update

--- server/src/routes/video.route.js
+++ server/src/routes/video.route.js
@@ -5,19 +5,17 @@ import multer from "multer";
 
 const router = express.Router();
 
-console.log("connection established successfully");
-let bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
-
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.get("/video/:fileId", upload.single("file"), (req, res) =>
-  videoController.getVideo(req, res, bucket)
-);
+router.get("/video/:fileId", upload.single("file"), (req, res) => {
+  bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
+  videoController.getVideo(req, res, bucket);
+});
 
-router.post("/video", upload.single("file"), (req, res) =>
-  videoController.addVideo(req, res, bucket)
-);
-// });
+router.post("/video", upload.single("file"), (req, res) => {
+  bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
+  videoController.addVideo(req, res, bucket);
+});
 
 export default router;
