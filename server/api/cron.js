23bed507d8774b1517ebf4f4b20f63c7955791f5
Dmytro Sabitov <templerun6375@gmail.com>
2024-05-10 17:51:36 +0300

    Update

--- server/api/cron.js
+++
@@ -1,7 +0,0 @@
-
-import telegramController from "../src/controllers/telegram.controller"
-
-export default function handler(req, res) {
-    telegramController.sendMessage();
-    res.status(200).end('!!!');
-}
